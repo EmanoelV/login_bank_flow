8d0a43e044b77d3a19d0eda8d2650b8155f419a4
Emanoel S. Vieira <dev.emanoel@gmail.com>
2021-08-01 21:43:40 -0300

    add transaction model

--- lib/app/data/models/transaction_model.dart
+++ lib/app/data/models/transaction_model.dart
@@ -1,1 +1,28 @@
-class TransactionModel {}
+class TransactionModel {
+  late String tipoLancamento;
+  late String descricaoOperacao;
+  late String dataMovimento;
+  late double valorMovimento;
+
+  TransactionModel(
+      {required this.tipoLancamento,
+      required this.descricaoOperacao,
+      required this.dataMovimento,
+      required this.valorMovimento});
+
+  TransactionModel.fromJson(Map<String, dynamic> json) {
+    tipoLancamento = json['tipoLancamento'];
+    descricaoOperacao = json['descricaoOperacao'];
+    dataMovimento = json['dataMovimento'];
+    valorMovimento = json['valorMovimento'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['tipoLancamento'] = this.tipoLancamento;
+    data['descricaoOperacao'] = this.descricaoOperacao;
+    data['dataMovimento'] = this.dataMovimento;
+    data['valorMovimento'] = this.valorMovimento;
+    return data;
+  }
+}
--- lib/app/modules/extract/extract_controller.dart
+++ lib/app/modules/extract/extract_controller.dart
@@ -1,4 +1,5 @@
 import 'package:get/get_state_manager/get_state_manager.dart';
+
 import 'package:login_bank_flow/app/data/models/models.dart';
 
 class ExtractController extends GetxController {
